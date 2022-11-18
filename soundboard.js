3208a5b4f1846711c578dea7764a7249dbd9cc31
Sara Barker <sarbark01@gmail.com>
2022-11-18 15:56:04 -0500

    tried changing to audio var

--- soundboard.js
+++ soundboard.js
@@ -1,13 +1,13 @@
-var buttonElement1 = document.getElementById('button-1')
-var buttonElement2 = document.getElementById('button-2')
-var buttonElement3 = document.getElementById('button-3')
+var buttonElement1 = document.getElementById('button-1');
+var buttonElement2 = document.getElementById('button-2');
+var buttonElement3 = document.getElementById('button-3');
 
-var bubbling = document.getElementById("bubbling1.wav")
-var coin = document.getElementById("coin.wav")
-var crash = document.getElementById("crash_x.wav")
+var bubbling = new Audio("bubbling1.wav");
+var coin = new Audio("coin.wav");
+var crash = new Audio("crash_x.wav");
 
 buttonElement1.addEventListener('click', function () {
-    console.log('I was clicked!')
+    console.log('I was clicked!');
     bubbling.play();
   })
 
@@ -17,7 +17,7 @@ buttonElement1.addEventListener('click', function () {
 
 
   buttonElement2.addEventListener('click', function () {
-    console.log('I was clicked!')
+    console.log('I was clicked!');
     coin.play();
   })
   buttonElement2.addEventListener('mouseenter', function () {
@@ -26,7 +26,7 @@ buttonElement1.addEventListener('click', function () {
 
 
   buttonElement3.addEventListener('click', function () {
-    console.log('I was clicked!')
+    console.log('I was clicked!');
     crash.play();
   })
   buttonElement3.addEventListener('mouseenter', function () {
