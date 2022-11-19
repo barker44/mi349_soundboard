bf69f96554cad4203f5dd925cfbf5719a27718ff
Sara Barker <sarbark01@gmail.com>
2022-11-19 14:25:19 -0500

    now that js is actually loading

--- soundboard.js
+++ soundboard.js
@@ -1,11 +1,33 @@
-console.log('I was clicked!')
 var buttonElement1 = document.getElementById('button-1');
 var buttonElement2 = document.getElementById('button-2');
 var buttonElement3 = document.getElementById('button-3');
 
-function play() {
-  alert("test");
-  new Audio("bubbling1.wav").play();
+function play(element) {
+  var audio = document.getElementById(element);
+  audio.play();
 }
 
-buttonElement1.addEventListener('click', play);
+
+buttonElement1.addEventListener('click', function(){
+  play("bubbling1.wav")
+  })
+
+  buttonElement1.addEventListener('mouseenter', function () {
+  play("bubbling1.wav")
+  })
+
+
+  buttonElement2.addEventListener('click', function () {
+  play("coin.wav")
+  })
+  buttonElement2.addEventListener('mouseenter', function () {
+  play("coin.wav")
+})
+
+
+  buttonElement3.addEventListener('click', function () {
+  play("crash_x.wav")
+  })
+  buttonElement3.addEventListener('mouseenter', function () {
+  play("crash_x.wav")
+})
