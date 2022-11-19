871c76095f489d4da45dc72819dde722449387d1
Sara Barker <sarbark01@gmail.com>
2022-11-19 14:02:40 -0500

    test

--- soundboard.js
+++ soundboard.js
@@ -2,10 +2,10 @@ var buttonElement1 = document.getElementById('button-1');
 var buttonElement2 = document.getElementById('button-2');
 var buttonElement3 = document.getElementById('button-3');
 
-function play(element) {
-  var audio = document.getElementById(element);
+function play() {
+  var audio = document.getElementById("bubbling.wav");
   audio.play();
   alert("test");
 }
 
-buttonElement1.addEventListener('click', play("bubbling.wav"));
+buttonElement1.addEventListener('click', play());
