ad3cfac4ce59508b771cc8b25d7f635ea4e40f09
Sara Barker <sarbark01@gmail.com>
2022-11-19 14:19:32 -0500

    script file not loading

--- soundboard.js
+++ soundboard.js
@@ -1,3 +1,4 @@
+console.log('I was clicked!')
 var buttonElement1 = document.getElementById('button-1');
 var buttonElement2 = document.getElementById('button-2');
 var buttonElement3 = document.getElementById('button-3');
