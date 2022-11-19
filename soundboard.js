ffe61882b2cc0adcd13b6c87b84ec1e2ea8e79dd
Sara Barker <sarbark01@gmail.com>
2022-11-19 14:07:52 -0500

    if i keep trying maybe itll work

--- soundboard.js
+++ soundboard.js
@@ -3,7 +3,7 @@ var buttonElement2 = document.getElementById('button-2');
 var buttonElement3 = document.getElementById('button-3');
 
 function play() {
-  var audio = document.getElementById("bubbling1.wav");
+  var audio = new Audio("bubbling1.wav");
   audio.play();
   alert("test");
 }
